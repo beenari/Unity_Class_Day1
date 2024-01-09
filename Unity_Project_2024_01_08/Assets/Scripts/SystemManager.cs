70a417f55762e3e80ab4e6f4996948ed7fe570aa
beenari <sdh230216@sdh.hs.kr>
2024-01-09 15:10:59 +0900

    2일차 끝

--- Unity_Project_2024_01_08/Assets/Scripts/SystemManager.cs
+++ Unity_Project_2024_01_08/Assets/Scripts/SystemManager.cs
@@ -15,25 +15,31 @@ public class SystemManager : MonoBehaviour
 
     public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if(player != null)
+        if (player != null)
         {
             spawnTime += Time.deltaTime;
-            if(nextspawnTime <= spawnTime)
+            if (nextspawnTime <= spawnTime)
             {
                 spawnTime = 0.0f;
-                nextspawnTime = Random.Range(0.5f, 2.0f);
+                nextspawnTime = Random.Range(0.5f, 2.0f);   //랜덤으로 다음 스폰 시간을 설정한다. 
+
+                int EnemyObjectsCount = EnemyObjects.Length;            //등록한 적 개체의 숫자를 가져온다.
+                int SpawntransformCount = spawntransform.Length;        //등록한 스폰 포인트의 갯수를 가져온다.
+
+                int RandEnemyObjectNumer = Random.Range(0, EnemyObjectsCount);      //가져온 숫자를 최대로 놓고 랜덤 숫자를 생성 
+                int RandSpawntransformNumer = Random.Range(0, SpawntransformCount); //가져온 숫자를 최대로 놓고 랜덤 숫자를 생성 
+
+                //해당 랜덤 숫자를 기반으로 등록된 몬스터 배열 번호와 스폰 포인트 번호에 위치에 적을 생성 시킨다. 
+                GameObject temp = (GameObject)Instantiate(
+                    EnemyObjects[RandEnemyObjectNumer], spawntransform[RandSpawntransformNumer].position, Quaternion.identity);
+
             }
 
-            if(player.transform.position.y < -50.0f)
+
+            if (player.transform.position.y < -50.0f)
             {
                 player.transform.position = Vector3.zero + new Vector3(0.0f, 1.0f, 0.0f);
                 player.transform.rotation = Quaternion.identity;
