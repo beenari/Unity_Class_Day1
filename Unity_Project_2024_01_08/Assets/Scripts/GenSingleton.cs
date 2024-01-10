e9c756dad482198acc53637fd8ecf507d682a4de
beenari <sdh230216@sdh.hs.kr>
2024-01-10 11:03:11 +0900

    UI 기능 테스트

--- Unity_Project_2024_01_08/Assets/Scripts/GenSingleton.cs
+++ Unity_Project_2024_01_08/Assets/Scripts/GenSingleton.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GenSingleton : GenericSingleton<GenSingleton>
 {
+    //public Text textUI;
+    public TMP_Text tmptextUI;
     public int playerScore = 0;
+    public int playerScoreMax = 100;
+    public Slider mainSlider;
 
     public void AddScore(int amount) 
     { 
         playerScore += amount; 
+        tmptextUI.text = playerScore.ToString();
+        mainSlider.value = (float)playerScore / (float)playerScoreMax;
+    }
+
+    public void SubmitSliderValue()
+    {
+
+        tmptextUI.text = mainSlider.value.ToString();
+    }
+
+    void Start()
+    {
+        tmptextUI.text = "22222222";
     }
 }
